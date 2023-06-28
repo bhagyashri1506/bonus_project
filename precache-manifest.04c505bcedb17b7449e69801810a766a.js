5cabed3d8b85b03d52c18d282046fb4fc27e41b3
Harshit <harshitupadhyay872@gmail.com>
2023-06-28 19:09:05 +0530

    Updates

---
+++ precache-manifest.04c505bcedb17b7449e69801810a766a.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5b1cc1eabaf994e1dc5",
+    "url": "/bonus_project/static/css/main.4c96ec8d.chunk.css"
+  },
+  {
+    "revision": "f5b1cc1eabaf994e1dc5",
+    "url": "/bonus_project/static/js/main.f5b1cc1e.chunk.js"
+  },
+  {
+    "revision": "6f6f4986655de22842af",
+    "url": "/bonus_project/static/js/1.6f6f4986.chunk.js"
+  },
+  {
+    "revision": "e6533c1c86934e295eb7",
+    "url": "/bonus_project/static/css/2.dfb3e0f4.chunk.css"
+  },
+  {
+    "revision": "e6533c1c86934e295eb7",
+    "url": "/bonus_project/static/js/2.e6533c1c.chunk.js"
+  },
+  {
+    "revision": "2729eef17f9a141ffabe",
+    "url": "/bonus_project/static/js/runtime~main.2729eef1.js"
+  },
+  {
+    "revision": "96199dfd0062258f9d32e7d3c94ee342",
+    "url": "/bonus_project/static/media/bonus-bg.96199dfd.png"
+  },
+  {
+    "revision": "016541ca4e6453b92df9c9ebffd74217",
+    "url": "/bonus_project/static/media/login.016541ca.png"
+  },
+  {
+    "revision": "bfc7457745838764adb5ceaa34d61b4e",
+    "url": "/bonus_project/static/media/deposit-btn.bfc74577.png"
+  },
+  {
+    "revision": "a250fe6ce67c91a3ec3f906cf72467e4",
+    "url": "/bonus_project/static/media/register.a250fe6c.png"
+  },
+  {
+    "revision": "09cb80f0c13cb7f9fd7ad7d0ce20e236",
+    "url": "/bonus_project/static/media/withdraw-btn.09cb80f0.png"
+  },
+  {
+    "revision": "88ab1e4aa07b45fbe1976340674e1070",
+    "url": "/bonus_project/static/media/refresh-btn.88ab1e4a.png"
+  },
+  {
+    "revision": "584cb3f8a1f864bc3393f59f034dff6f",
+    "url": "/bonus_project/static/media/share.584cb3f8.gif"
+  },
+  {
+    "revision": "bed92eada8664db84732f84892404b03",
+    "url": "/bonus_project/static/media/download.bed92ead.png"
+  },
+  {
+    "revision": "6dd7e98e795b5405ac7ce5147d6f9cc4",
+    "url": "/bonus_project/static/media/info.6dd7e98e.png"
+  },
+  {
+    "revision": "c7f7c1b8795a0f8dfe4b391d71f87176",
+    "url": "/bonus_project/static/media/link.c7f7c1b8.png"
+  },
+  {
+    "revision": "725f1abf5fa1d4b4055d3efbeb3c327e",
+    "url": "/bonus_project/static/media/refer.725f1abf.jpg"
+  },
+  {
+    "revision": "a856a434377eb19ac44a3830098dd60e",
+    "url": "/bonus_project/static/media/soccer.a856a434.jpg"
+  },
+  {
+    "revision": "cb3241fb1bba6571010fec37cab04f63",
+    "url": "/bonus_project/static/media/marquee.cb3241fb.gif"
+  },
+  {
+    "revision": "8f22aef2b7f6e6b3bc0e92e5432155d6",
+    "url": "/bonus_project/static/media/movie.8f22aef2.png"
+  },
+  {
+    "revision": "64ac689dfd94cb47fa8ac326f6ca03fe",
+    "url": "/bonus_project/static/media/header.64ac689d.gif"
+  },
+  {
+    "revision": "8b7a686d459421ee26a6ceec7d2b2b2e",
+    "url": "/bonus_project/static/media/game1.8b7a686d.png"
+  },
+  {
+    "revision": "6494021ffe95190d023021e56a063149",
+    "url": "/bonus_project/static/media/game2.6494021f.png"
+  },
+  {
+    "revision": "0953f1633150757d4619a815a955a790",
+    "url": "/bonus_project/static/media/game3.0953f163.png"
+  },
+  {
+    "revision": "dc491f489922de33e79ea080db29fe88",
+    "url": "/bonus_project/static/media/game4.dc491f48.png"
+  },
+  {
+    "revision": "d01eb359ec60242fe4fe20922fb006aa",
+    "url": "/bonus_project/static/media/game5.d01eb359.png"
+  },
+  {
+    "revision": "694e8f24f69935f0a403d35a761cdc2c",
+    "url": "/bonus_project/static/media/game6.694e8f24.png"
+  },
+  {
+    "revision": "82db6e189c0bce0a0a588b016e553e0d",
+    "url": "/bonus_project/static/media/home.82db6e18.png"
+  },
+  {
+    "revision": "51df133d97264da2e47f0e0ce8e46b94",
+    "url": "/bonus_project/static/media/history.51df133d.png"
+  },
+  {
+    "revision": "799a5df4e2b1c7f2051227866d73da4f",
+    "url": "/bonus_project/static/media/livechat.799a5df4.png"
+  },
+  {
+    "revision": "5013f9bee19c5b159cddf4043156b8e2",
+    "url": "/bonus_project/static/media/setting.5013f9be.png"
+  },
+  {
+    "revision": "b387331207064ceb32f8e163d7c35a50",
+    "url": "/bonus_project/static/media/bank_banner.b3873312.png"
+  },
+  {
+    "revision": "4ee2abe4425e66f65a0cafee3157063d",
+    "url": "/bonus_project/static/media/login-page.4ee2abe4.png"
+  },
+  {
+    "revision": "c1faec50c5a4e8828917b09e8bdef9c1",
+    "url": "/bonus_project/static/media/deposite-card1.c1faec50.png"
+  },
+  {
+    "revision": "8236a3e36554cdef85a4b85bdbcf0eee",
+    "url": "/bonus_project/static/media/deposite-card2.8236a3e3.png"
+  },
+  {
+    "revision": "b75fc039330013c1603df15f0ad9f49a",
+    "url": "/bonus_project/static/media/deposite-card3.b75fc039.png"
+  },
+  {
+    "revision": "2ace3a3588f891443e56c1e39728fe44",
+    "url": "/bonus_project/index.html"
+  }
+];
